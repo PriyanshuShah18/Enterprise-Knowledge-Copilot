93287a8dfc373c511ff3360143ecc986c1c60b78
PriyanshuShah18 <priyanshu18shah@gmail.com>
2026-02-05 16:48:34 +0530

    GROQ Fallback

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,20 @@
 import os
+
+#print("CWD:", os.getcwd())
+
 from dotenv import load_dotenv
 
 load_dotenv()
+#print("GROQ_API_KEY loaded:", bool(os.getenv("GROQ_API_KEY")))
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
-if not GOOGLE_API_KEY:
+
+
+if not GOOGLE_API_KEY and not GROQ_API_KEY:
     raise RuntimeError(
-        "GOOGLE_API_KEY is not set. "
-        "Add it to your .env file (local) or Railway environment variables."
+        "At least one LLM API key must be set"
     )
 
 UPLOAD_DIR = "data/uploads"
--- backend/rag/chain.py
+++ backend/rag/chain.py
@@ -2,18 +2,32 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import re
 
-from backend.config import GOOGLE_API_KEY
+from backend.config import GOOGLE_API_KEY,GROQ_API_KEY
 from backend.rag.vectorstore import get_retriever
 
 
 # -------- LLM (instantiate ONCE) --------
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash-lite",
-    temperature=0.1,
-    max_output_tokens=512,
-    google_api_key=GOOGLE_API_KEY,
-)
+def get_llm():
+    if GOOGLE_API_KEY:
+        try:
+            return ChatGoogleGenerativeAI(
+                model="gemini-2.5-flash",
+                temperature=0.1,
+                max_output_tokens=512,
+                google_api_key=GOOGLE_API_KEY,
+            )
+        except Exception as e:
+            print("Gemini init failed,Falling back to GROQ:",e)
+    if GROQ_API_KEY:
+        return ChatGroq(
+            api_key=GROQ_API_KEY,
+            model="qwen/qwen3-32b",
+            max_tokens=512,
+        )
+    raise RuntimeError("No working LLM provider available")
 
 
 prompt = ChatPromptTemplate.from_template(
@@ -35,7 +49,14 @@ prompt = ChatPromptTemplate.from_template(
 )
 
 
+def clean_llm_output(text:str)->str:
+    # Remove <think>  from Groq response
+    text=re.sub(r"<think>.*</think>","",text,flags=re.DOTALL)
+    return text.strip()
+
+
 _rag_chain = None
+
 
 
 def get_rag_chain():
@@ -50,8 +71,9 @@ def get_rag_chain():
                 "question": RunnablePassthrough(),
             }
             | prompt
-            | llm                     # ✅ correct
+            | get_llm()                     # ✅ correct
             | StrOutputParser()
+            | clean_llm_output
         )
 
     return _rag_chain
